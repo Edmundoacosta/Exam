380794e2a1b0b589f0ceb22c003fabb39d66d066
Edmundo Acosta <edmundoach@hotmail.com>
2018-12-06 22:09:16 +0100

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'exam';
+	public data;
+	public departments:Array<any> = [];
+	public provinces:Array<any> = [];
+	public districts:Array<any> = [];
+	
+	public getObject(data){
+		if (data.trim() == '') return null
+		data = data.trim().split(' ');
+		return {
+			code: data[0],
+			name: data[1],
+			parent: {}
+		}
+	}
+	constructor(private http: HttpClient) {
+		this.http.get('../assets/data.txt', {responseType: 'text'}).subscribe( res => {
+			this.data = res.split(/\r\n|\n/);
+			for (var i = 0; i < this.data.length; i++) {
+				let info = this.data[i].split('"')[1];
+				let parts = info.split("/");
+				let department = this.getObject(parts[0]);
+				department.parent['code'] = '-';
+				department.parent['name'] = '-';
+				let validateDepartment = this.departments.find(dep => dep.code == department.code);
+				if (!validateDepartment) {
+					this.departments.push(department);
+				}
+				let province = this.getObject(parts[1]);
+				if (province) {
+					province.parent = department;
+					let validateProvince = this.provinces.find(prov =>  prov.code == province.code);
+					if (!validateProvince) {
+						this.provinces.push(province);
+					}
+					let district = this.getObject(parts[2]);
+					if (district) {
+						district.parent = province;
+						let validateDistrict = this.districts.find(dis => dis.code == district.code);
+						if (!validateDistrict) {
+							this.districts.push(district);
+						}
+					}
+				}
+			}
+		});
+	}
 }
